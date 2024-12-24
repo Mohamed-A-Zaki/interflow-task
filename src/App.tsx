ba1647453ff8a1c31a19fcfc853e362efec91a9b
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-12-24 10:39:11 +0200

    refactor App component structure; create HomePage component to encapsulate layout and improve organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import AddBranchButton from "./components/AddBranchButton";
-import BranchesList from "./components/BranchesList";
-import BreadcrumbSection from "./components/BreadcrumbSection";
-import Footer from "./components/Footer";
 import AddBranchModal from "./components/modals/AddBranchModal";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <div dir="rtl">
-      <div className="flex">
-        <Sidebar />
-        <div className="container">
-          <Navbar />
-          <main>
-            <BreadcrumbSection />
-            <AddBranchButton />
-            <BranchesList />
-          </main>
-        </div>
-      </div>
-      <Footer />
+      <HomePage />
 
       {/* modals */}
       <AddBranchModal />
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import AddBranchButton from "@/components/AddBranchButton";
+import BranchesList from "@/components/BranchesList";
+import BreadcrumbSection from "@/components/BreadcrumbSection";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+export default function HomePage() {
+  return (
+    <>
+      <div className="flex">
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <main>
+            <BreadcrumbSection />
+            <AddBranchButton />
+            <BranchesList />
+          </main>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
