74b18af9a41ccd07918889873d614598c8855ed8
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-12-24 06:27:53 +0200

    add BranchItem component; implement Checkbox and Switch UI components; update BranchesList layout and styles; add new Radix UI dependencies

---
+++ src/components/BranchItem.tsx
@@ -0,0 +1,34 @@
+import { FaStore } from "react-icons/fa";
+import { Badge } from "./ui/badge";
+import { Checkbox } from "./ui/checkbox";
+import { Switch } from "./ui/switch";
+
+export default function BranchItem() {
+  return (
+    <div className="mb-5 flex items-center justify-between border-b pb-5">
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center gap-2">
+          <span>الرئيسي</span>
+          <Badge
+            variant={"outline"}
+            className="rounded-full border-secondary text-primary"
+          >
+            مفتوح
+          </Badge>
+        </div>
+        <div className="flex items-center gap-2 text-sm">
+          <div className="flex items-center gap-1">
+            <FaStore />
+            مستودع
+          </div>
+          -
+          <div className="flex items-center gap-1">
+            <Checkbox className="rounded-full" />
+            <span className="text-primary underline">الافتراضي</span>
+          </div>
+        </div>
+      </div>
+      <Switch dir="ltr" className="data-[state=checked]:bg-secondary" />
+    </div>
+  );
+}
--- src/components/BranchesList.tsx
+++ src/components/BranchesList.tsx
@@ -1,7 +1,34 @@
+import { Infinity } from "lucide-react";
+import { FaStore } from "react-icons/fa";
+import BranchItem from "./BranchItem";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 export default function BranchesList() {
   return (
-    <>
-      <h1>BranchesList</h1>
-    </>
+    <div className="overflow-hidden rounded-lg border">
+      <div className="flex items-center justify-between bg-[#fcfcfc] p-5">
+        <div className="flex items-center gap-2">
+          <FaStore />
+          <span className="font-semibold text-[#666]">الفروع والمستودعات</span>
+          <span className="text-sm text-gray-400">(1 من 5)</span>
+        </div>
+        <div className="flex overflow-hidden rounded-md">
+          <Input
+            className="rounded-none rounded-s-md focus-visible:ring-0"
+            placeholder="عدد لا محدود"
+          />
+          <Button className="rounded-none bg-secondary/70 text-primary hover:bg-secondary">
+            <Infinity />
+          </Button>
+        </div>
+      </div>
+
+      <div className="p-5">
+        {[...new Array(3)].map((_, indx) => {
+          return <BranchItem key={indx} />;
+        })}
+      </div>
+    </div>
   );
 }
