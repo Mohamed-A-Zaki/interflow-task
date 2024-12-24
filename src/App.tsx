ca9967c8ab71a85e2b03b7df6126f250b5abb814
Mohamed Zaki <87452598+Mohamed-A-Zaki@users.noreply.github.com>
2024-12-24 10:05:08 +0200

    add Footer component and restructure App layout for improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import AddBranchButton from "./components/AddBranchButton";
 import BranchesList from "./components/BranchesList";
 import BreadcrumbSection from "./components/BreadcrumbSection";
+import Footer from "./components/Footer";
 import AddBranchModal from "./components/modals/AddBranchModal";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 export default function App() {
   return (
-    <div dir="rtl" className="flex">
-      <Sidebar />
-      <div className="container">
-        <Navbar />
-        <main>
-          <BreadcrumbSection />
-          <AddBranchButton />
-          <BranchesList />
-        </main>
+    <div dir="rtl">
+      <div className="flex">
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <main>
+            <BreadcrumbSection />
+            <AddBranchButton />
+            <BranchesList />
+          </main>
+        </div>
       </div>
+      <Footer />
 
       {/* modals */}
       <AddBranchModal />
